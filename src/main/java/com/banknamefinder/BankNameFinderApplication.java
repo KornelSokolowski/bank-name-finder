3268dcbbad18945bb1412dc045709466524b8708
Kornel <example@example.com>
2023-11-03 10:55:31 +0100

    init

---
+++ src/main/java/com/banknamefinder/BankNameFinderApplication.java
@@ -0,0 +1,13 @@
+package com.banknamefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankNameFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankNameFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banknamefinder/BankNameFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banknamefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankNameFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
